6b30923cf4970275b035948dc6b48827790bb7ad
Sivasamy <sivasamyboxy18@gmail.com>
2025-07-25 14:43:10 +0530

    slot create time validation added

--- app/Http/Controllers/AdSlotController.php
+++ app/Http/Controllers/AdSlotController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\AdSlot;
+use Carbon\Carbon;
 
 
 class AdSlotController extends Controller
@@ -18,11 +19,15 @@ class AdSlotController extends Controller
     }
 
     public function store(Request $request) {
+        $now = Carbon::now();
         $request->validate([
             'name' => 'required|string',
-            'start_time' => 'required|date',
-            'end_time' => 'required|date|after:start_time',
+            'start_time' => ['required', 'date', 'after:' . $now],
+            'end_time' => ['required', 'date', 'after:start_time'],
             'min_bid_price' => 'required|numeric|min:0',
+        ], [
+            'start_time.after' => 'Start time must be in the future.',
+            'end_time.after' => 'End time must be after the start time.',
         ]);
 
         return AdSlot::create($request->all());
